8ba90595e35df566965f82797269aebeb9504092
acassaigne <anthony.cassaigne@gmail.com>
2020-11-07 15:23:59 +0100

    first iteration python

--- test_life_game.py
+++ test_life_game.py
@@ -1,6 +1,35 @@
 import unittest
 
+
+class Grid(object):
+    def __init__(self):
+        self.cell = False
+        self.grid = {}
+
+    def is_alive(self, x, y):
+        return self.grid[(x,y)]
+
+    def set_alive(self, x, y):
+        self.grid[(x,y)] = True
+
+
 class TestMain(unittest.TestCase):
 
-    def test_main(self):
-        self.assertEqual(True, True)
+    def test_cell_is_dead(self):
+        grid = Grid()
+        result = grid.is_alive(0,0)
+        self.assertEqual(False, result)
+
+    def test_cell_is_alive(self):
+        grid = Grid()
+        grid.set_alive(0,0)
+        result = grid.is_alive(0,0)
+        self.assertEqual(True, result)
+
+    def test_3(self):
+        grid = Grid()
+        grid.set_alive(0,1)
+        result = grid.is_alive(0,0)
+        self.assertEqual(False, result)
+
+
