2c3cc6cf75eb0b72fd28026a770134debad3c76e
Yann Irrilo <15703478+yirrilo@users.noreply.github.com>
2020-11-07 16:13:17 +0100

    nouveau test sur le false dead

--- test_life_game.py
+++ test_life_game.py
@@ -1,7 +1,17 @@
 import unittest
 
 class Grid():
+
+    def __init__(self):
+        pass
+
     def is_dead(self, x, y):
+        return True
+
+    def set_alive(self, x, y):
+        self.cell = True
+
+    def is_alive(self, x, y):
         return True
 
 class TestMain(unittest.TestCase):
@@ -14,6 +24,11 @@ class TestMain(unittest.TestCase):
         grid = Grid()
         grid.set_alive(0,0)
         self.assertTrue(grid.is_alive(0,0))
+        
+    def test_3(self):
+        grid = Grid()
+        grid.set_alive(0,0)
+        self.assertFalse(grid.is_dead(0,0))
 
 if __name__ == '__main__':
     unittest.main()
