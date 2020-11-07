3ca755d34c391f8e07851dcd5e59a0289078ddbc
Olivier Lemaitre <o.lemaitre@welcloud.io>
2020-11-07 15:01:31 +0000

    test cell is alive

--- test_life_game.py
+++ test_life_game.py
@@ -1,7 +1,19 @@
 import unittest
 
+class Grid():
+    def is_dead(self, x, y):
+        return True
+
 class TestMain(unittest.TestCase):
 
-    def test_1(self):
+    def test_cell_is_dead(self):
         grid = Grid()
         self.assertTrue(grid.is_dead(0,0))
+        
+    def test_cell_is_alive(self):
+        grid = Grid()
+        grid.set_alive(0,0)
+        self.assertTrue(grid.is_alive(0,0))
+
+if __name__ == '__main__':
+    unittest.main()
